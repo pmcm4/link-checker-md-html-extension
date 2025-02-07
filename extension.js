d41298245bb0018039fa1f57d7c856cc3f7121e0
pmorato <pmorato187@gmail.com>
2025-02-07 21:58:02 +0800

    Revert "Rename extension to "link-checker-md-html-tc" and update related commands in README and package.json"
    This reverts commit ed528abde102184e0c6c1e83ee5084b50a08bb81.

--- extension.js
+++ extension.js
@@ -13,9 +13,9 @@ function activate(context) {
     console.log('URL Checker extension is now active!');
 
     // Create a diagnostic collection for reporting problems
-    diagnosticCollection = vscode.languages.createDiagnosticCollection('link-checker-md-html-tc');
+    diagnosticCollection = vscode.languages.createDiagnosticCollection('link-checker');
 
-    let disposable = vscode.commands.registerCommand('link-checker-md-html-tc.checkUrls', async () => {
+    let disposable = vscode.commands.registerCommand('link-checker.checkUrls', async () => {
         const editor = vscode.window.activeTextEditor;
 
         if (!editor) {
